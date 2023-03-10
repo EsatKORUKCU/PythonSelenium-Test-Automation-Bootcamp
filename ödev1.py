81c30d08e30f8e5949d723fad9478a4905cce820
Esat KÖRÜKCÜ <105414787+EsatKORUKCU@users.noreply.github.com>
2023-03-10 11:45:56 +0300

    Add files via upload

---
+++ ödev1.py
@@ -0,0 +1,20 @@
+# 1 -) Veri tipleri
+# int = sayısal degerler için kullanılır.
+# string = metinsel degerler için kullanılır.
+# floaat = Ondalıklı sayılar için kullamılır.
+# boolean = True vuya false degerleri için kullanılır.
+# List = içerisinde farklı veri türlerini aynı anda kullanılır.
+
+
+
+# 2 - ) soru 
+# Ders isimleri string veri tiplerine örnek: Yazılımcı gelistirici yetistirme kapı
+# Ders tamamlanma oranı int veri tiplerine örmektir. 
+
+# 3-) soru girip ya
+# if (kullniciAdi == grlnkullniciAdi   and sifre == grlnsifre ):
+#     print("Kullanıcı giriş yaptı")
+# elif (kullniciAdi != grlnkullniciAdi or sifre!= grlnsifre  ):
+#     print("Hatalı giriş")
+# else:
+#     print("invalid data")
