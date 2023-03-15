c40733ed6c4fbf8801d5e8bca8a9a483c9a8797f
Esat KÖRÜKCÜ <105414787+EsatKORUKCU@users.noreply.github.com>
2023-03-15 21:37:04 +0300

    Add files via upload

---
+++ ödev/person.py
@@ -0,0 +1,13 @@
+class person:
+    def __init__(self,name,lastname):
+        self.name =  name
+        self.lastName = lastname
+    
+    
+    
+    
+    
+musteri1 = person("Ahmet" , "Demiroğ")
+musteri2 = person("Kerem" , "varış")
+musteri3 = person("İlker" , "Tural")
+print(musteri1.name)
